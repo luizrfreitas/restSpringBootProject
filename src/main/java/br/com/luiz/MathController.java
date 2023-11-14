5dc2c628cbd33de61fbdcc1fc7700920463970b6
Luiz G R Freitas <luiz.freitas@meisters.solutions>
2023-11-14 16:56:11 -0300

    wraping up

--- src/main/java/br/com/luiz/MathController.java
+++ src/main/java/br/com/luiz/MathController.java
@@ -2,20 +2,23 @@ package br.com.luiz;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class GreetingsController {
+public class MathController {
 
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 	
-	@RequestMapping("/greetings")
-	public Greetings greeting(
-			@RequestParam(value = "name", defaultValue = "World")
-			String name) {
-		return new Greetings(counter.incrementAndGet(), String.format(template, name));
+	@RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method=RequestMethod.GET)
+	public Double sum(
+			@PathVariable(value = "numberOne") String numberOne,
+			@PathVariable(value = "numberOne") String numberTwo
+		) {
+		return 1D;
 	}
 }
